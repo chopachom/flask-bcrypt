b781889e1c553440faca3bff5d65b02f2a7bbbbb
Alexander Petrovich <chopachom@gmail.com>
2011-08-08 10:41:14 +0400

    Fixing rounds argument for generate_password_hash (_log_rounds is mutable, see http://docs.python.org/reference/compound_stmts.html#function-definitions)

--- flaskext/bcrypt.py
+++ flaskext/bcrypt.py
@@ -8,22 +8,26 @@ def bcrypt_init(app):
     if rounds:
         _log_rounds[0] = rounds
 
-def generate_password_hash(password, rounds=_log_rounds[0]):
-    '''Generates a password hash using `bcrypt`. Specifying `log_rounds` sets 
-    the log_rounds parameter of `bcrypt.gensalt()` which determines the 
+def generate_password_hash(password, rounds=None):
+    '''Generates a password hash using `bcrypt`. Specifying `log_rounds` sets
+    the log_rounds parameter of `bcrypt.gensalt()` which determines the
     complexity of the salt. 12 is the default value.
-    
+
     Returns the hashed password.
     '''
-    
+
+    if rounds is None:
+        rounds = _log_rounds[0]
+
     if not password:
         raise ValueError('Password must be non-empty.')
-    
+
     password = str(password)
-    
+
     pw_hash = bcrypt.hashpw(password, bcrypt.gensalt(rounds))
-    
+
     return pw_hash
+
 
 def constant_time_compare(val1, val2):
     '''Returns True if the two strings are equal, False otherwise.
